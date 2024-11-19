0dcc8c8daabf198a6806578935f351388a37da40
William Giammona <wgiammona@gmail.com>
2024-11-19 19:40:08 +0200

    links and logo added

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,22 +1,32 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { Contrast } from "lucide-react";
 
 const Nav = () => {
   return (
-    <nav className="font-sans">
+    <nav className="font-sans flex justify-between w-full max-w-6xl mx-auto h-24 items-center px-14">
       <figure>
         <Image src="/images/logo.png" alt="Logo" width={50} height={50} />
       </figure>
-      <ul className="nav__link-list">
-        <li>
-          <Link href="/">About</Link>
+      <ul className="flex items-center">
+        <li className="mx-3">
+          <Link className="text-[#242424] font-bold" href="/">
+            About
+          </Link>
+        </li>
+        <li className="mx-3">
+          <Link className="text-[#242424] font-bold" href="/projects">
+            Projects
+          </Link>
+        </li>
+        <li className="mx-3">
+          <Link className="text-[#242424] font-bold" href="/contact">
+            Contact
+          </Link>
         </li>
         <li>
-          <Link href="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact</Link>
+          <Contrast className="text-xl" />
         </li>
       </ul>
     </nav>
