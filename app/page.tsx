1553b0fdd5b8f3dbf7ddc651cdfa3a4f784d8f28
William Giammona <wgiammona@gmail.com>
2024-11-19 18:43:59 +0200

    imported Nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Nav from "./components/Nav";
+
 export default function Home() {
   return (
     <>
-      <section id="landing-page"></section>
+      <section id="landing-page">
+        <Nav />
+      </section>
     </>
   );
 }
