ed901b7bab9ef4ea9feab3abb8ef3db7bffd4bb6
William Giammona <wgiammona@gmail.com>
2024-11-19 19:45:24 +0200

    imported Header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Nav from "./components/Nav";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
     <>
       <section className="min-h-screen">
         <Nav />
+        <Header />
       </section>
     </>
   );
