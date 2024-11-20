237b9fe7ed4ea43660eaa8acac775efca4053ae2
William Giammona <wgiammona@gmail.com>
2024-11-20 16:47:47 +0200

    name change and Mailbtn import

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Nav from "./components/Nav";
-import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Mailbtn from "./components/Mailbtn";
 
 export default function Home() {
   return (
     <>
       <section className="min-h-screen">
         <Nav />
-        <Header />
+        <Hero />
+        <Mailbtn />
       </section>
     </>
   );
