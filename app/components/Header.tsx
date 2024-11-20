7a519d1e0e9fb4964c662dbc1da0a3de1484bf7b
William Giammona <wgiammona@gmail.com>
2024-11-20 15:23:45 +0200

    adding icons

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,39 @@
 import React from "react";
+import { Linkedin } from "lucide-react";
+import { Github } from "lucide-react";
+import { FileText } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold w-full max-w-5xl">
-      <div>
-        <h1>Hey</h1>
-        <h1>I&apos;m William</h1>
-        <p>
-          I&apos;m a Frontend Software Engineer with a strong passion for
-          building web applications with great user experiences.
-        </p>
+    <header className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold w-full max-w-5xl px-8">
+      <h1 className="text-8xl mb-3 text-left">Hey</h1>
+      <h1 className="text-orange text-8xl mb-3 text-left">I&apos;m William.</h1>
+      <p className="text-xl max-w-96 leading-[2]">
+        I&apos;m a <b className="text-orange">Frontend Software Engineer </b>
+        with a strong passion for building web applications with great user
+        experiences.
+        <br />
+        Here&apos;s a bit more <b className="text-orange">about me.</b>
+      </p>
+      <div className="flex mt-4 gap-3">
+        <Link
+          href="https://www.linkedin.com/in/william-giammona-933376339/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Linkedin className="bg-orange text-white rounded-lg p-2" size={32} />
+        </Link>
+        <Link
+          href="https://github.com/WilliamGiammona"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Github className="bg-orange text-white p-2 rounded-lg" size={32} />
+        </Link>
+        <Link href="/" target="_blank" rel="noopener noreferrer">
+          <FileText className="bg-orange text-white p-2 rounded-lg" size={32} />
+        </Link>
       </div>
     </header>
   );
