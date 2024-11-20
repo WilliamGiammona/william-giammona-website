1a94485e6a901cb0a8c770c62c90503e618f0a1b
William Giammona <wgiammona@gmail.com>
2024-11-20 14:00:01 +0200

    added text/centering

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold w-full max-w-5xl">
+      <div>
+        <h1>Hey</h1>
+        <h1>I&apos;m William</h1>
+        <p>
+          I&apos;m a Frontend Software Engineer with a strong passion for
+          building web applications with great user experiences.
+        </p>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
