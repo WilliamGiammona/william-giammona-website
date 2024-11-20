55f29fa4fdb2594c19b71ba327370bf39e73ffe6
William Giammona <wgiammona@gmail.com>
2024-11-20 17:19:14 +0200

    border and scroll btn

--- app/components/Scroll.tsx
+++ app/components/Scroll.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Scroll = () => {
-  return <div>Scroll</div>;
+  return (
+    <div
+      className="
+  w-5 
+  h-8
+  border-2 
+  border-solid 
+  border-mail
+  rounded-3xl
+  flex 
+  items-center 
+  justify-center
+  absolute 
+  bottom-5 
+  left-1/2 
+  -translate-x-1/2
+  after:content-[''] 
+  after:absolute 
+  after:w-1
+  after:h-1 
+  after:bg-mail
+"
+    ></div>
+  );
 };
 
 export default Scroll;
