a6ee8328b12e02c21d79f04375a2ea4b9984edad
William Giammona <wgiammona@gmail.com>
2024-11-20 18:30:19 +0200

    initial commit

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import Image from "next/image";
 
 const Projects = () => {
   return (
     <>
-      <div className="mx-auto">
-        <h1>
-          Here are some of my <span className="text-orange">projects</span>
+      <div className="w-full max-w-6xl mx-auto px-3">
+        <h1 className="text-center text-5xl mb-16">
+          <b>
+            Here are some of my <span className="text-orange">projects</span>
+          </b>
         </h1>
+        <ul>
+          <li></li>
+        </ul>
       </div>
     </>
   );
