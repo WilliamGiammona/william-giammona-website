7c15f79b43877682c4e4fb8e16775c5af5124385
William Giammona <wgiammona@gmail.com>
2024-11-20 16:59:27 +0200

    finished

--- app/components/Mailbtn.tsx
+++ app/components/Mailbtn.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Mail } from "lucide-react";
 
 const Mailbtn = () => {
-  return <div>Mailbtn</div>;
+  return (
+    <div>
+      <Mail
+        className="text-mail rounded-lg cursor-pointer absolute bottom-4 right-4 "
+        size={70}
+      />
+    </div>
+  );
 };
 
 export default Mailbtn;
