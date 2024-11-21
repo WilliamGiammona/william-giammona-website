0a56be8ee899fa353f818960270b38da8d60957e
William Giammona <wgiammona@gmail.com>
2024-11-21 13:44:09 +0200

    added links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Github } from "lucide-react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <figure>
+        <Image src="/images/logo.png" alt="Logo" width={50} height={50} />
+      </figure>
+      <ul>
+        <li>
+          <Link href="/">Github</Link>
+        </li>
+        <li>
+          <Link href="/">LinkedIn</Link>
+        </li>
+        <li>
+          <Link href="/">Email</Link>
+        </li>
+        <li>
+          <Link href="/">Resume</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default Footer;
