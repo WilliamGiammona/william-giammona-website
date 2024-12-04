0a37ee42dbd8b3972ff3fb8bd97ff16b4533ed0c
William Giammona <wgiammona@gmail.com>
2024-12-04 17:07:02 +0200

    added scrollToTop

--- app/components/Scroll.tsx
+++ app/components/Scroll.tsx
@@ -1,7 +1,23 @@
+"use client";
+
 import React from "react";
 
 const Scroll = () => {
-  return <div className="scroll"></div>;
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div
+      className="scroll"
+      onClick={scrollToTop}
+      role="button"
+      aria-label="Scroll to top"
+    />
+  );
 };
 
 export default Scroll;
