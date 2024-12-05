91bc493a952ada12459b697846427ba5760264b4
William Giammona <wgiammona@gmail.com>
2024-12-05 18:09:17 +0200

    made responsive adjustments

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,17 +9,17 @@ const Footer = () => {
   const { setIsFormVisible } = useModal();
 
   return (
-    <section className="relative flex flex-col items-center z-10">
+    <section className="relative flex flex-col items-center z-10 px-4">
       <figure>
         <Image
           src="/images/logo.png"
           alt="Logo"
           width={70}
           height={70}
-          className="dark:invert"
+          className="dark:invert w-[50px] h-[50px] md:w-[70px] md:h-[70px]"
         />
       </figure>
-      <ul className="flex w-full justify-around max-w-lg text-primary my-5">
+      <ul className="flex flex-wrap justify-center md:justify-around gap-4 md:gap-0 w-full max-w-lg text-primary my-3 md:my-5 text-sm md:text-base">
         <li className="relative">
           <Link
             className="link-footer relative"
@@ -63,7 +63,7 @@ const Footer = () => {
           </Link>
         </li>
       </ul>
-      <div className="text-primary">
+      <div className="text-primary text-sm md:text-base">
         <span className="text-xs">&#9400;</span> Made By William Giammona
       </div>
     </section>
