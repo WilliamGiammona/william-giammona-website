7468cbef617f5c801428ccb7965740f88a2d552c
William Giammona <wgiammona@gmail.com>
2024-12-05 14:35:13 +0200

    updated md and lg animations

--- app/components/BackgroundShapes.tsx
+++ app/components/BackgroundShapes.tsx
@@ -7,29 +7,46 @@ const SCALE_FACTOR = 1 / 20;
 
 const BackgroundShapes = () => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [isXlScreen, setIsXlScreen] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsXlScreen(window.innerWidth >= 1280); // xl breakpoint
+    };
+
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);
+
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
 
   useEffect(() => {
     const handleMouseMove = (event: MouseEvent) => {
-      const x = event.clientX * SCALE_FACTOR;
-      const y = event.clientY * SCALE_FACTOR;
-      setMousePosition({ x, y });
+      if (isXlScreen) {
+        const x = event.clientX * SCALE_FACTOR;
+        const y = event.clientY * SCALE_FACTOR;
+        setMousePosition({ x, y });
+      }
     };
 
     window.addEventListener("mousemove", handleMouseMove);
-
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, []);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, [isXlScreen]);
 
   return (
     <div className="fixed inset-0 pointer-events-none overflow-hidden z-[1] hidden md:block">
       {/* Top Row */}
       <div
-        className="absolute top-32 left-32 transition-transform duration-300 ease-out"
-        style={{
-          transform: `translate(${mousePosition.x}px, ${mousePosition.y}px)`,
-        }}
+        className={`absolute top-32 left-32 transition-transform duration-300 ease-out ${
+          !isXlScreen && "animate-float1"
+        }`}
+        style={
+          isXlScreen
+            ? {
+                transform: `translate(${mousePosition.x}px, ${mousePosition.y}px)`,
+              }
+            : undefined
+        }
       >
         <Image
           src="/images/backgroundImages/semi-circle.svg"
@@ -40,10 +57,18 @@ const BackgroundShapes = () => {
       </div>
 
       <div
-        className="absolute top-32 left-1/2 -translate-x-1/2 transition-transform duration-300 ease-out"
-        style={{
-          transform: `translate(${-mousePosition.x}px, ${mousePosition.y}px)`,
-        }}
+        className={`absolute top-32 left-1/2 -translate-x-1/2 transition-transform duration-300 ease-out ${
+          !isXlScreen && "animate-float2"
+        }`}
+        style={
+          isXlScreen
+            ? {
+                transform: `translate(${-mousePosition.x}px, ${
+                  mousePosition.y
+                }px)`,
+              }
+            : undefined
+        }
       >
         <Image
           src="/images/backgroundImages/circle.svg"
@@ -54,10 +79,18 @@ const BackgroundShapes = () => {
       </div>
 
       <div
-        className="absolute top-32 right-32 transition-transform duration-300 ease-out"
-        style={{
-          transform: `translate(${mousePosition.x}px, ${-mousePosition.y}px)`,
-        }}
+        className={`absolute top-32 right-32 transition-transform duration-300 ease-out ${
+          !isXlScreen && "animate-float3"
+        }`}
+        style={
+          isXlScreen
+            ? {
+                transform: `translate(${
+                  mousePosition.x
+                }px, ${-mousePosition.y}px)`,
+              }
+            : undefined
+        }
       >
         <Image
           src="/images/backgroundImages/squiggly.svg"
@@ -69,10 +102,18 @@ const BackgroundShapes = () => {
 
       {/* Middle Row */}
       <div
-        className="absolute top-1/2 -translate-y-1/2 left-32 transition-transform duration-300 ease-out"
-        style={{
-          transform: `translate(${-mousePosition.x}px, ${mousePosition.y}px)`,
-        }}
+        className={`absolute top-1/2 -translate-y-1/2 left-32 transition-transform duration-300 ease-out ${
+          !isXlScreen && "animate-float4"
+        }`}
+        style={
+          isXlScreen
+            ? {
+                transform: `translate(${-mousePosition.x}px, ${
+                  mousePosition.y
+                }px)`,
+              }
+            : undefined
+        }
       >
         <Image
           src="/images/backgroundImages/semi-circle.svg"
@@ -83,10 +124,18 @@ const BackgroundShapes = () => {
       </div>
 
       <div
-        className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 transition-transform duration-300 ease-out"
-        style={{
-          transform: `translate(${mousePosition.x}px, ${-mousePosition.y}px)`,
-        }}
+        className={`absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 transition-transform duration-300 ease-out ${
+          !isXlScreen && "animate-float5"
+        }`}
+        style={
+          isXlScreen
+            ? {
+                transform: `translate(${
+                  mousePosition.x
+                }px, ${-mousePosition.y}px)`,
+              }
+            : undefined
+        }
       >
         <Image
           src="/images/backgroundImages/triangle.svg"
@@ -97,10 +146,16 @@ const BackgroundShapes = () => {
       </div>
 
       <div
-        className="absolute top-1/2 -translate-y-1/2 right-32 transition-transform duration-300 ease-out"
-        style={{
-          transform: `translate(${-mousePosition.x}px, ${-mousePosition.y}px)`,
-        }}
+        className={`absolute top-1/2 -translate-y-1/2 right-32 transition-transform duration-300 ease-out ${
+          !isXlScreen && "animate-float6"
+        }`}
+        style={
+          isXlScreen
+            ? {
+                transform: `translate(${-mousePosition.x}px, ${-mousePosition.y}px)`,
+              }
+            : undefined
+        }
       >
         <Image
           src="/images/backgroundImages/circle.svg"
@@ -112,10 +167,18 @@ const BackgroundShapes = () => {
 
       {/* Bottom Row */}
       <div
-        className="absolute bottom-32 left-32 transition-transform duration-300 ease-out"
-        style={{
-          transform: `translate(${mousePosition.x}px, ${-mousePosition.y}px)`,
-        }}
+        className={`absolute bottom-32 left-32 transition-transform duration-300 ease-out ${
+          !isXlScreen && "animate-float7"
+        }`}
+        style={
+          isXlScreen
+            ? {
+                transform: `translate(${
+                  mousePosition.x
+                }px, ${-mousePosition.y}px)`,
+              }
+            : undefined
+        }
       >
         <Image
           src="/images/backgroundImages/squiggly.svg"
@@ -126,10 +189,18 @@ const BackgroundShapes = () => {
       </div>
 
       <div
-        className="absolute bottom-32 left-1/2 -translate-x-1/2 transition-transform duration-300 ease-out"
-        style={{
-          transform: `translate(${-mousePosition.x}px, ${mousePosition.y}px)`,
-        }}
+        className={`absolute bottom-32 left-1/2 -translate-x-1/2 transition-transform duration-300 ease-out ${
+          !isXlScreen && "animate-float8"
+        }`}
+        style={
+          isXlScreen
+            ? {
+                transform: `translate(${-mousePosition.x}px, ${
+                  mousePosition.y
+                }px)`,
+              }
+            : undefined
+        }
       >
         <Image
           src="/images/backgroundImages/circle.svg"
@@ -140,10 +211,16 @@ const BackgroundShapes = () => {
       </div>
 
       <div
-        className="absolute bottom-32 right-32 transition-transform duration-300 ease-out"
-        style={{
-          transform: `translate(${mousePosition.x}px, ${mousePosition.y}px)`,
-        }}
+        className={`absolute bottom-32 right-32 transition-transform duration-300 ease-out ${
+          !isXlScreen && "animate-float9"
+        }`}
+        style={
+          isXlScreen
+            ? {
+                transform: `translate(${mousePosition.x}px, ${mousePosition.y}px)`,
+              }
+            : undefined
+        }
       >
         <Image
           src="/images/backgroundImages/semi-circle.svg"
